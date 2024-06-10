2685851b4509cf445938fdb73bb8ec88a6a30450
Oscar Perez <operez@zoni.edu>
2024-06-10 16:06:44 -0400

    Enhance Container Initialization and Cleanup Robustness

--- run_tests.sh
+++ run_tests.sh
@@ -15,11 +15,14 @@ stop() {
 }
 
 init() {
-  mkdir /tmp/storage
-  $CONTAINER_BIN run --rm -d -p 6379:6379 --name test_redis redis:alpine
-  $CONTAINER_BIN run -it --rm -d -p 7700:7700 --name test_meili getmeili/meilisearch:v0.28.0
-  $CONTAINER_BIN volume create classquiz_db_data
-  $CONTAINER_BIN run --name classquiz_db -p 5432:5432 --rm -d -e POSTGRES_PASSWORD=mysecretpassword -v classquiz_db_data:/var/lib/postgresql/data -e POSTGRES_DB=classquiz postgres
+  [ -d /tmp/storage ] || mkdir /tmp/storage
+  $CONTAINER_BIN container exists test_redis && $CONTAINER_BIN container rm -f test_redis
+  $CONTAINER_BIN run --rm -d -p 6379:6379 --name test_redis docker.io/library/redis:alpine
+  $CONTAINER_BIN container exists test_meili && $CONTAINER_BIN container rm -f test_meili
+  $CONTAINER_BIN run -it --rm -d -p 7700:7700 --name test_meili docker.io/getmeili/meilisearch:v0.28.0
+  $CONTAINER_BIN volume exists classquiz_db_data || $CONTAINER_BIN volume create classquiz_db_data
+  $CONTAINER_BIN container exists classquiz_db && $CONTAINER_BIN container rm -f classquiz_db
+  $CONTAINER_BIN run --name classquiz_db -p 5432:5432 --rm -d -e POSTGRES_PASSWORD=mysecretpassword -v classquiz_db_data:/var/lib/postgresql/data -e POSTGRES_DB=classquiz docker.io/library/postgres
   sleep 1
   pipenv run alembic upgrade head
 }
