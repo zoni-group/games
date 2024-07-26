91d095c4e2b8a4d8e09c7e79f38b478608f6067b
Oscar Perez <operez@zoni.edu>
2024-07-26 18:39:24 -0400

    Improve activity control and answers submited counter

--- frontend/src/lib/play/admin/controls.svelte
+++ frontend/src/lib/play/admin/controls.svelte
@@ -51,68 +51,57 @@ SPDX-License-Identifier: MPL-2.0
         set_question_number(0);
     });
 </script>
-
+<style>
+	.slide-control-btn {
+	  @apply bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75;
+	}
+  </style>
 <div
 	class="  w-full h-10 mt-4 px-4 z-20 grid grid-cols-2"
 	style="background: {bg_color ? bg_color : 'transparent'}"
 	class:text-black={bg_color}
 >
-	<p class="absolute top-10 left-10 z-50 mr-auto ml-0 col-start-1 col-end-1 flex items-center italic justify-center font-semibold bg-white text-black dark:text-black rounded-full w-20 border-8 border-[#0AEDFE] h-20">
+	<p class="fixed top-10 left-10 z-50 mr-auto ml-0 col-start-1 col-end-1 flex items-center italic justify-center font-semibold bg-white text-black dark:text-black rounded-full w-20 border-8 border-[#0AEDFE] h-20">
 		{selected_question === -1 ? '0' : selected_question + 1}
 		/ {quiz_data.questions.length}
 	</p>
-	<div class="justify-self-end ml-auto mr-0 col-start-3 col-end-3">
-		<div class="flex flex-col justify-center items-center" >
-			
-			{#if selected_question + 1 === quiz_data.questions.length && ((timer_res === '0' && question_results !== null) || quiz_data?.questions?.[selected_question]?.type === QuizQuestionType.SLIDE)}
-				{#if JSON.stringify(final_results) === JSON.stringify([null])}
-					<button on:click={get_final_results} class="text-white  underline"
-						>{$t('admin_page.get_final_results')}
-					</button>
-				{/if}
-			{:else if timer_res === '0' || selected_question === -1}
-				{#if (selected_question + 1 !== quiz_data.questions.length && question_results !== null) || selected_question === -1}
-					<button
-						on:click={() => {
-							set_question_number(selected_question + 1);
-						}}
-						class="text-white  underline "
-						>{$t('admin_page.next_question', { question: selected_question + 2 })}
-					</button>
-				{/if}
-				{#if question_results === null && selected_question !== -1}
-					{#if quiz_data.questions[selected_question].type === QuizQuestionType.SLIDE}
-						<button
-							on:click={() => {
-								set_question_number(selected_question + 1);
-							}}
-							class="text-white  underline"
-							>{$t('admin_page.next_question', { question: selected_question + 2 })}
-						</button>
-					{:else}
-						<button on:click={get_question_results} class="text-white  underline"
-							>{$t('admin_page.show_results')}
-						</button>
-					{/if}
-				{/if}
-			{:else if selected_question !== -1}
-				{#if quiz_data.questions[selected_question].type === QuizQuestionType.SLIDE}
-					<button
-						on:click={() => {
-							set_question_number(selected_question + 1);
-						}}
-						class="text-white  underline"
-						>{$t('admin_page.next_question', { question: selected_question + 2 })}
-					</button>
-				{:else}
-					<button on:click={show_solutions} class="text-white  underline"
-						>{$t('admin_page.stop_time_and_solutions')}
-					</button>
-				{/if}
+	<div class="fixed top-4 right-20 z-50 flex flex-col items-center space-y-4">
+		{#if selected_question + 1 === quiz_data.questions.length && ((timer_res === '0' && question_results !== null) || quiz_data?.questions?.[selected_question]?.type === QuizQuestionType.SLIDE)}
+		  {#if JSON.stringify(final_results) === JSON.stringify([null])}
+			<button on:click={get_final_results} class="slide-control-btn">
+			  {$t('admin_page.get_final_results')}
+			</button>
+		  {/if}
+		{:else if timer_res === '0' || selected_question === -1}
+		  {#if (selected_question + 1 !== quiz_data.questions.length && question_results !== null) || selected_question === -1}
+			<button on:click={() => set_question_number(selected_question + 1)} class="slide-control-btn">
+			  {$t('admin_page.next_question', { question: selected_question + 2 })}
+			</button>
+		  {/if}
+		  {#if question_results === null && selected_question !== -1}
+			{#if quiz_data.questions[selected_question].type === QuizQuestionType.SLIDE}
+			  <button on:click={() => set_question_number(selected_question + 1)} class="slide-control-btn">
+				{$t('admin_page.next_question', { question: selected_question + 2 })}
+			  </button>
+			{:else}
+			  <button on:click={get_question_results} class="slide-control-btn">
+				{$t('admin_page.show_results')}
+			  </button>
 			{/if}
-			<p class=" text-lg border-2 px-4 py-2 mx-3 mt-4 underline rounded-xl border-[#0AEDFE] text-white">
-				{$t('admin_page.answers_submitted', { answer_count: answer_count })}
-			</p>
-		</div>
-	</div>
+		  {/if}
+		{:else if selected_question !== -1}
+		  {#if quiz_data.questions[selected_question].type === QuizQuestionType.SLIDE}
+			<button on:click={() => set_question_number(selected_question + 1)} class="slide-control-btn">
+			  {$t('admin_page.next_question', { question: selected_question + 2 })}
+			</button>
+		  {:else}
+			<button on:click={show_solutions} class="slide-control-btn">
+			  {$t('admin_page.stop_time_and_solutions')}
+			</button>
+		  {/if}
+		{/if}
+		<p class=" text-lg px-4 py-2 mx-3 mt-4 rounded-xl text-white">
+			{$t('admin_page.answers_submitted', { answer_count: answer_count })}
+		</p>
+	  </div>
 </div>
