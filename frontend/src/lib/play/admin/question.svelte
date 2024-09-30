9be37e35b97cb0e588f77c750f11fdaea760e23e
AbhishekBante <abante@zoni.com>
2024-09-30 10:10:39 -0400

    fix oversized width of options

--- frontend/src/lib/play/admin/question.svelte
+++ frontend/src/lib/play/admin/question.svelte
@@ -77,7 +77,7 @@ SPDX-License-Identifier: MPL-2.0
 		<div class="grid grid-rows-2 grid-flow-col auto-cols-auto gap-2 w-4/5 lg:w-full ps-1 ">
 			{#each quiz_data.questions[selected_question].answers as answer, i}
 				<div
-					class="rounded-lg h-full flex border-2 border-[#0056BD] items-center flex-grow lg:h-[20vh] lg:w-[48vw] lg:max-h-[20vh] lg:max-w-[48vw] w-[30vw] transition-all"
+					class="rounded-lg h-full flex border-2 border-[#0056BD] items-center flex-grow lg:min-h-[7vh] lg:min-w-full lg:max-h-[20vh] lg:max-w-[48vw] w-[30vw] transition-all"
 					style="background-color: {answer.color ?? default_colors[i]};"
 					class:opacity-50={!answer.right &&
 						timer_res === '0' &&
